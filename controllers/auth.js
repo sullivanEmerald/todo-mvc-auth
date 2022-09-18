0cf6a6d974a1ff97b8b7c15ed21e63909677f69b
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-09-18 23:13:53 +0100

    fixed logout issue in the auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,13 +36,14 @@ const User = require('../model/User')
   }
   
   exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
     req.session.destroy((err) => {
       if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
       res.redirect('/')
+    })
+
+    req.logout(() => {
+      console.log('User has logged out.')
     })
   }
   
